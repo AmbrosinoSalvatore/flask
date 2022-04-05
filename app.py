4f052b50c1f4ec48f648ccf0efe338b351a06221
sasyvelobelo <ambrosino.salvatore@iisgalvanimi.edu.it>
2022-04-05 11:15:20 +0000

    ff

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ import geopandas as gpd
 import io
 import pandas as pd
 stazioni = pd.read_csv('/workspace/flask/templates/coordfix_ripetitori_radiofonici_milano_160120_loc_final.csv',sep=';')
+stazionigeo = gpd.read_file('/workspace/flask/ds710_coordfix_ripetitori_radiofonici_milano_160120_loc_final.geojson',sep=';')
+quartieri = gpd.read_file('/workspace/flask/ds964_nil_wm.zip')
 @app.route('/', methods=['GET'])
 def home():
     return render_template('home.html')
@@ -23,7 +25,7 @@ def selezione():
     elif scelta == 'es2':
         return redirect(url_for('input'))
     else:
-        redirect(url_for('dropdown'))
+        return redirect(url_for('dropdown'))
 
 @app.route('/numero', methods=['GET'])
 def numero():
@@ -46,7 +48,40 @@ def grfico():
     FigureCanvas(fig).print_png(output)#stampare l'immagiine sull'output
     return Response(output.getvalue(), mimetype='image/png')#mandare in risposta quello che c'e nell'output e gli diciamo che è un immagine
 
+@app.route('/ricerca', methods=['GET'])
+def ricerca():
+  quartiere = request.args['quartieri']
+  quartiereUtente = quartieri[quartieri['NIL'].str.contains(quartiere)]
+  stazioniQuartieri = stazionigeo[stazionigeo.within(quartiereUtente.geometry.squeeze())]
+  return render_template('elenco1.html', risultato = stazioniQuartieri.to_html())
 
+@app.route('/input', methods=['GET'])
+def input():
+    return render_template('input.html')
+
+@app.route('/dropdown', methods=['GET'])
+def dropdown():
+    nomistazioni = stazioni.OPERATORE.to_list()
+    nomistazioni = list(set(nomistazioni))
+    nomistazioni.sort()
+    return render_template('dropdown.html', stazioni = nomistazioni)
+
+@app.route('/sceltastazioni', methods=['GET'])
+def sceltastazioni():
+    stazione = request.args['stazione']
+    stazione_utente = stazionigeo[stazionigeo.OPERATORE == stazione]
+    quartiere = quartieri[quartieri.contains(stazione_utente.geometry.squeeze())]
+    return render_template(lista_stazione.html,quartiere=quartiere)
+
+@app.route('/mappaquarieri', methods=['GET'])
+def mappaquart():
+  fig, ax = plt.subplots(figsize = (12,8))
+  quartiere1.to_crs(epsg=3857).plot(ax=ax, alpha=0.5, edgecolor='k')
+  stazioni_utente.to_crs(epsg=3857).plot(ax=ax, color='r', edgecolor='k')
+  ctx.add_basemap(ax=ax)
+  output = io.BytesIO()
+  FigureCanvas(fig).print_png(output)
+  return Response(output.getvalue(), mimetype='image/png')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
